2415d96eb0aec036cc959d06895f2dec3e220ad9
Sophia Guan <sophiaguan@dhcp-10-29-244-212.dyn.MIT.EDU>
2025-09-13 18:15:45 -0400

    fix

--- server/upload.js
+++ server/upload.js
@@ -1,7 +1,16 @@
+import dotenv from "dotenv";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import fs from "fs"; // For Node.js, local file
 
-const s3 = new S3Client({ region: "us-east-1" });
+dotenv.config();
+
+const s3 = new S3Client({
+    region: "us-east-1",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
 
 async function uploadImage(filePath, clothingType, fileName) {
   // filePath: local path from AI pipeline output
